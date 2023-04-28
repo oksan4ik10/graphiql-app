3cc11e1917a60e1fd711d6a6ac48833c1f9a3726
Olya Y <f_akora@mail.ru>
2023-04-28 18:09:21 +0300

    feat: add changing style of nested in NavLink button component when NavLink is active

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,23 +5,65 @@ import Button from '../Utils/Button/Button';
 
 export function SignUp() {
   return (
-    <NavLink to="/signup" className={style.header_nav_item}>
-      <Button buttonText="Sign Up" buttonType="button" />
-    </NavLink>
+    <NavLink
+      to="/signup"
+      className={style.header_nav_item}
+      children={({ isActive }) => (
+        <Button
+          buttonText="Sign Up"
+          buttonType="button"
+          buttonStyle={isActive ? 'alternate_back' : ''}
+        />
+      )}
+    />
   );
 }
 
 export function SignIn() {
   return (
-    <NavLink to="/signin" className={style.header_nav_item}>
-      <Button buttonText="Sign In" buttonType="button" />
-    </NavLink>
+    <NavLink
+      to="/signin"
+      className={style.header_nav_item}
+      children={({ isActive }) => (
+        <Button
+          buttonText="Sign In"
+          buttonType="button"
+          buttonStyle={isActive ? 'alternate_back' : ''}
+        />
+      )}
+    />
   );
 }
 
 //пока что кнопка просто ведёт назад на WelcomePage
-export function LogOut() {}
+export function LogOut() {
+  return (
+    <NavLink
+      to="/"
+      className={style.header_nav_item}
+      children={({ isActive }) => (
+        <Button
+          buttonText="Log Out"
+          buttonType="button"
+          buttonStyle={isActive ? 'alternate_back' : ''}
+        />
+      )}
+    />
+  );
+}
 
-export function GoToMain() {}
-
-console.log(Button);
+export function GoToMain() {
+  return (
+    <NavLink
+      to="/main"
+      className={style.header_nav_item}
+      children={({ isActive }) => (
+        <Button
+          buttonText="To Main"
+          buttonType="button"
+          buttonStyle={isActive ? 'alternate_back' : ''}
+        />
+      )}
+    />
+  );
+}
--- src/components/Utils/Button/Button.tsx
+++ src/components/Utils/Button/Button.tsx
@@ -1,17 +1,14 @@
-import style from './Button.module.css';
+import './Button.css';
 
 interface IButtonProps {
   buttonText: string;
   buttonType: 'button' | 'submit' | 'reset' | undefined;
-  buttonStyle?: string;
+  buttonStyle?: 'alternate_back' | '';
 }
 
 export default function Button({ buttonText, buttonType, buttonStyle }: IButtonProps) {
   return (
-    <button
-      className={buttonStyle ? `${buttonStyle} ${style.button}` : style.button}
-      type={buttonType}
-    >
+    <button className={buttonStyle ? `button ${buttonStyle}` : 'button'} type={buttonType}>
       {buttonText}
     </button>
   );
