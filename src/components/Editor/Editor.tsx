9dc2df436ff22185240042a8aa2ddad0bafae4af
Kalinina Oksana <ksan4ik92@gmail.com>
2023-05-01 16:26:43 +0300

    feat: add read text with code

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,9 +1,18 @@
 import CodeMirror from '@uiw/react-codemirror';
+import { ChangeEvent, useState } from 'react';
 
 export default function Editor() {
+  const [value, setValue] = useState('');
+
+  function changeInput(e: ChangeEvent<HTMLInputElement>) {
+    const text = e.target.outerText;
+    setValue(text);
+    console.log(value);
+  }
+
   return (
     <CodeMirror
-      value="console.log('hello world!');"
+      value={value}
       height="60vh"
       basicSetup={{
         foldGutter: false,
@@ -11,6 +20,7 @@ export default function Editor() {
         allowMultipleSelections: false,
         indentOnInput: false,
       }}
+      onInput={changeInput}
     />
   );
 }
