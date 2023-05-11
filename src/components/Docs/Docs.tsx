545202301ca9732a5d4dfe6ffc3f15789f2ff9a2
anastasia <anastasia.gribovskaia@gmail.com>
2023-05-12 00:34:05 +0300

    feat: incorrect probability of obtaining schema

--- src/components/Docs/Docs.tsx
+++ src/components/Docs/Docs.tsx
@@ -1,47 +1,115 @@
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
+
+// import styles from './Docs.module.css';
+// import { useAppSelector } from '../../store/store';
+// import {
+//   getIntrospectionQuery,
+//   buildClientSchema,
+//   printSchema,
+//   buildASTSchema,
+// } from 'graphql/utilities';
+// import { parse } from 'graphql';
+
+// async function getSchema(endpoint: string) {
+//   const response = await fetch(endpoint, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ query: getIntrospectionQuery() }),
+//   });
+//   const graphqlSchemaObj = buildClientSchema((await response.json()).data);
+//   try {
+//     return graphqlSchemaObj;
+//     // return printSchema(graphqlSchemaObj);
+//   } finally {
+//   }
+// }
+
+// const schema = await getSchema('https://countries.trevorblades.com');
+// // console.log(schema);
+// // console.log(schema?.getTypeMap()); //получить все типы
+
+// function DocsThree() {
+//   const root = schema?.getQueryType();
+//   const rootFieldsObj = schema?.getQueryType()?.getFields() as object;
+//   const rootFieldsKeys = Object.keys(rootFieldsObj);
+//   // const rootFieldsValues = Object.values(rootFieldsObj);
+//   // console.log(schema?.getQueryType()?.getFields().continent);
+//   // console.log(rootFieldsValues);
+
+//   const listItems = rootFieldsKeys.map((item) => <li key={item}>{item}</li>);
+//   console.log(listItems);
+
+//   const { t } = useTranslation();
+
+//   return (
+//     <ul>
+//       <>
+//         {root?.toString()}
+//         {listItems}
+//       </>
+//     </ul>
+//   );
+// }
+
+// ----------------------------------------------------------------------------------------
+
+// import { useTranslation } from 'react-i18next';
 
 import styles from './Docs.module.css';
 import { useAppSelector } from '../../store/store';
-import { getIntrospectionQuery, buildClientSchema, printSchema } from 'graphql/utilities';
+import {
+  getIntrospectionQuery,
+  buildASTSchema,
+  printSchema,
+  buildClientSchema,
+  typeFromAST,
+} from 'graphql/utilities';
+import { parse, parseType } from 'graphql';
 
 async function getSchema(endpoint: string) {
   const response = await fetch(endpoint, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ query: getIntrospectionQuery() }),
-  });
-  const graphqlSchemaObj = buildClientSchema((await response.json()).data);
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      // console.log(JSON.stringify(res.data, null, '\t'));
+      return res.data;
+      // return JSON.stringify(res.data, null, '\t');
+    });
   try {
-    return graphqlSchemaObj;
-    // return printSchema(graphqlSchemaObj);
+    return response;
   } finally {
   }
 }
 
 const schema = await getSchema('https://countries.trevorblades.com');
-// console.log(schema);
-// console.log(schema?.getTypeMap()); //получить все типы
+const schemaAst = buildASTSchema(parse(printSchema(buildClientSchema(schema))));
+const root = schemaAst?.getQueryType();
+const currentPath = root;
+
+// console.log(printSchema(schemaAst));
+// const json = JSON.stringify(printSchema(schemaAst));
+// console.log(json);
+
+// const printSchemaAst = printSchema(schemaAst);
+// const typeMap = schemaAst.getTypeMap();
+// console.log(typeMap);
+
+function showChildren(event: any) {
+  const children = currentPath?.getFields();
+}
 
 function DocsThree() {
-  const root = schema?.getQueryType();
-  const rootFieldsObj = schema?.getQueryType()?.getFields() as object;
-  const rootFieldsKeys = Object.keys(rootFieldsObj);
-  // const rootFieldsValues = Object.values(rootFieldsObj);
-  // console.log(schema?.getQueryType()?.getFields().continent);
-  // console.log(rootFieldsValues);
-
-  const listItems = rootFieldsKeys.map((item) => <li key={item}>{item}</li>);
-  console.log(listItems);
-
-  const { t } = useTranslation();
+  // const root = schemaAst?.getQueryType();
+  const rootName = root?.toString() as string;
+  console.log(root?.getFields());
 
   return (
-    <ul>
-      <>
-        {root?.toString()}
-        {listItems}
-      </>
-    </ul>
+    <div className={styles.node} onClick={showChildren}>
+      {rootName}
+    </div>
   );
 }
 
